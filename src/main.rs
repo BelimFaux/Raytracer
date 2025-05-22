c68fd5d0b06a2a2a8019183d8ba04da8e6547bce
Felix Baum <baumf03@unet.univie.ac.at>
2025-05-22 20:20:26 +0200

    feat: output a black image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 1920;
+    let height = 1080;
+
+    let mut imgbuf = image::ImageBuffer::new(width, height);
+
+    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = image::Rgb([0u8, 0u8, 0u8]);
+    }
+
+    imgbuf.save("output/test.png").unwrap();
 }
