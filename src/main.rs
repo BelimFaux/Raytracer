cb20c3364f4a7e4bdfd1e1be4903bb6c82318741
Felix Baum <baumf03@unet.univie.ac.at>
2025-05-23 13:17:38 +0200

    feat(math): Add Vector3, Point3 and Color utility

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use math::Color;
+
+mod math;
+
 fn main() {
-    let width = 1920;
-    let height = 1080;
+    let width = 256;
+    let height = 256;
 
     let mut imgbuf = image::ImageBuffer::new(width, height);
 
-    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = image::Rgb([0u8, 0u8, 0u8]);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = x as f32 / (width - 1) as f32;
+        let g = y as f32 / (height - 1) as f32;
+        let b = 0.2;
+        let color = Color::new(r, g, b);
+
+        *pixel = color.to_rgb();
     }
 
     imgbuf.save("output/test.png").unwrap();
---
+++ src/math/vector3.rs
@@ -0,0 +1,270 @@
+use std::ops;
+
+/// Struct to represent a 3D-Vector
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Vector3(f32, f32, f32);
+
+/// A point in 3D space
+pub type Point3 = Vector3;
+
+/// A color value with 3 floats representing red, green and blue
+pub type Color = Vector3;
+
+impl Color {
+    /// Convert a color with value in range 0 to 1 to an RGB value with values from 0 to 255
+    pub fn to_rgb(self) -> image::Rgb<u8> {
+        debug_assert!((0.0..=1.0).contains(&self.0));
+        debug_assert!((0.0..=1.0).contains(&self.1));
+        debug_assert!((0.0..=1.0).contains(&self.2));
+
+        let r = (255.999 * self.0) as u8;
+        let g = (255.999 * self.1) as u8;
+        let b = (255.999 * self.2) as u8;
+        image::Rgb([r, g, b])
+    }
+}
+
+impl Vector3 {
+    /// Create a new Vector from 3 floats
+    pub fn new(x: f32, y: f32, z: f32) -> Vector3 {
+        Vector3(x, y, z)
+    }
+
+    /// Creates a Vector with all components = 0
+    pub fn zero() -> Vector3 {
+        Vector3(0., 0., 0.)
+    }
+
+    /// computes the dot product
+    pub fn dot(&self, rhs: &Vector3) -> f32 {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+
+    /// computes the cross product of self x rhs (not commutative)
+    pub fn cross(&self, rhs: &Vector3) -> Vector3 {
+        Vector3(
+            self.1 * rhs.2 - self.2 * rhs.1,
+            self.2 * rhs.0 - self.0 * rhs.2,
+            self.0 * rhs.1 - self.1 * rhs.0,
+        )
+    }
+
+    /// returnes the length of the vector
+    pub fn length(&self) -> f32 {
+        f32::sqrt(self.length_squared())
+    }
+
+    /// returnes the square of the length of the vector
+    /// more efficient for comparisons
+    pub fn length_squared(&self) -> f32 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
+
+    /// normalize the vector
+    pub fn normalize(&mut self) {
+        *self /= self.length();
+    }
+}
+
+// --- Operators ---
+
+impl ops::Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+impl ops::AddAssign for Vector3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+        self.2 += rhs.2;
+    }
+}
+
+impl ops::Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector3(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    }
+}
+
+impl ops::SubAssign for Vector3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.0 -= rhs.0;
+        self.1 -= rhs.1;
+        self.2 -= rhs.2;
+    }
+}
+
+impl ops::Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Self::Output {
+        Vector3(-self.0, -self.1, -self.2)
+    }
+}
+
+impl ops::Mul<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vector3(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl ops::Mul<Vector3> for f32 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl ops::MulAssign<f32> for Vector3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0 *= rhs;
+        self.1 *= rhs;
+        self.2 *= rhs;
+    }
+}
+
+impl ops::Div<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        self * (1. / rhs)
+    }
+}
+
+impl ops::Div<Vector3> for f32 {
+    type Output = Vector3;
+
+    fn div(self, rhs: Vector3) -> Self::Output {
+        rhs / self
+    }
+}
+
+impl ops::DivAssign<f32> for Vector3 {
+    fn div_assign(&mut self, rhs: f32) {
+        *self *= 1. / rhs;
+    }
+}
+
+// --- Tests ---
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn vector_add_2_vectors() {
+        let v1 = Vector3::new(1., 2., 3.);
+        let v2 = Vector3::new(4., 5., 6.);
+
+        let res = v1 + v2;
+        let expected = Vector3::new(5., 7., 9.);
+
+        assert_eq!(res, expected);
+
+        let zero = Vector3::zero();
+
+        assert_eq!(expected + zero, expected);
+
+        // assign
+        let mut v1 = Vector3::new(1., 2., 3.);
+        let v2 = Vector3::new(4., 5., 6.);
+        v1 += v2;
+
+        let expected = Vector3::new(5., 7., 9.);
+
+        assert_eq!(v1, expected);
+    }
+
+    #[test]
+    fn vector_subtract_2_vectors() {
+        let v1 = Vector3::new(1., 2., 3.);
+        let v2 = Vector3::new(4., 5., 6.);
+
+        let res = v2 - v1;
+        let expected = Vector3::new(3., 3., 3.);
+
+        assert_eq!(res, expected);
+
+        let zero = Vector3::zero();
+
+        assert_eq!(expected - zero, expected);
+
+        // assign
+        let v1 = Vector3::new(1., 2., 3.);
+        let mut v2 = Vector3::new(4., 5., 6.);
+        v2 -= v1;
+
+        let expected = Vector3::new(3., 3., 3.);
+
+        assert_eq!(v2, expected);
+    }
+
+    #[test]
+    fn vector_mul_and_div() {
+        let v1 = Vector3::new(1., 2., 3.);
+
+        let res = Vector3::new(2., 4., 6.);
+
+        assert_eq!(v1 * 2., res);
+        assert_eq!(v1 * 2., 2. * v1);
+
+        assert_eq!(res / 2., v1);
+        assert_eq!(res / 2., 2. / res);
+
+        let mut v2 = Vector3::new(4., 5., 6.);
+        v2 *= 2.;
+
+        let res = Vector3::new(8., 10., 12.);
+        assert_eq!(v2, res);
+
+        v2 /= 2.;
+
+        assert_eq!(v2, res / 2.);
+    }
+
+    #[test]
+    fn vector_dot_and_cross() {
+        let v1 = Vector3::new(1., 2., 3.);
+        let v2 = Vector3::new(4., 5., 6.);
+
+        let exp_dot = 32.;
+
+        assert_eq!(v1.dot(&v2), exp_dot);
+        assert_eq!(v2.dot(&v1), exp_dot);
+
+        let exp_cross = Vector3::new(-3., 6., -3.);
+
+        assert_eq!(v1.cross(&v2), exp_cross);
+        assert_eq!(v2.cross(&v1), -exp_cross);
+    }
+
+    #[test]
+    fn vector_length() {
+        let mut v1 = Vector3::new(1., 2., 2.);
+
+        assert_eq!(v1.length_squared(), 9.);
+        assert_eq!(v1.length(), 3.);
+
+        v1.normalize();
+
+        assert_eq!(v1.length(), 1.);
+    }
+
+    #[test]
+    fn convert_color_to_rgb() {
+        let color = Color::new(1., 0.5, 0.); // Orange
+        let pixel = color.to_rgb();
+
+        let expected = image::Rgb([255, 127, 0]);
+
+        assert_eq!(pixel, expected);
+    }
+}
